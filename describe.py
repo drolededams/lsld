163d850a19f1e07a48fd7c58c2d1b1a283047da7
Damien GAMEIRO <dgameiro@e1r12p19.42.fr>
2019-02-26 13:37:12 +0100

    Bonus range

--- describe.py
+++ describe.py
@@ -2,6 +2,7 @@ import sys
 import math
 import numpy as np
 import pandas as pd
+import pprint
 
 
 def get_data(args):
@@ -28,26 +29,26 @@ def describe(data):
     stats['mean'] = sum(clean_data.values()) / count
     stats['var'] = 1 / (count - 1) * np.sum(np.power(np.subtract(values, stats['mean']), 2))
     stats['std'] = np.sqrt(stats['var'])
-    if isinstance(count * 0.25, int):
-        stats['25%'] = values[int(count * 0.25) - 1]
-    else:
-        stats['25%'] = values[math.floor(count * 0.25) - 1] + (values[math.floor(count * 0.25)] - values[math.floor(count * 0.25) - 1]) * ((count * 0.25) - math.floor(count * 0.25))
-    if count % 2 != 0:
-        stats['50%'] = values[int((count + 1) / 2 - 1)]
-    else:
+    stats['min'] = values[0]
+    x = 0.25 * (count - 1)
+    stats['25%'] = values[math.floor(x)] + (values[math.floor(x) + 1] - values[math.floor(x)]) * (x % 1)
+    if count % 2 == 0:
         stats['50%'] = (values[int(count / 2 - 1)] + values[int(count / 2)]) / 2
-    stats['75%'] = values[math.ceil((3 * count) / 4)]
+    else:
+        stats['50%'] = values[int((count + 1) / 2 - 1)]
+    x = 0.75 * (count - 1)
+    stats['75%'] = values[math.floor(x)] + (values[math.floor(x) + 1] - values[math.floor(x)]) * (x % 1)
+    stats['max'] = values[count - 1]
+    stats['range'] = stats['max'] - stats['min']
     return stats
 
 
 if __name__ == '__main__':
     df = get_data(sys.argv)
     df = df.select_dtypes('number')
-    print(df.columns)
     dd = df.to_dict()
     dict_desc = {column: describe(sub_dict) for column, sub_dict in dd.items()}
-    # if 'Index' in df.columns:
-    #     print("Index column present")
     pd.set_option('display.expand_frame_repr', False)
-    print(df.describe())
-    print(dict_desc)
+    res = pd.DataFrame.from_dict(dict_desc)
+    res = res.reindex(['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max', 'var', 'range'])
+    print(res)
