eb74891796f7f99471f47b17081ff80943890512
Damien GAMEIRO <dgameiro@e1r10p1.42.fr>
2019-02-28 16:17:36 +0100

    hist v0.2

--- histogram.py
+++ histogram.py
@@ -20,35 +20,47 @@ def get_data(args):
     return data
 
 
+def get_subject_grade_house(df, subject, house):
+    return df.loc[df['Hogwarts House'] == house, subject].dropna()
+
+
 if __name__ == '__main__':
     df = get_data(sys.argv)
     houses = df['Hogwarts House'].unique().tolist()
     print(houses)
 # Create a histogram. replace Herbology by good subject
-    for house in houses:
-        grade = df.loc[df['Hogwarts House'] == house, 'Arithmancy'].dropna()
-        plt.hist(grade, alpha=0.5, label=house)
-    plt.legend(loc='upper right')
-    plt.show()
+    # for house in houses:
+    #     grade = df.loc[df['Hogwarts House'] == house, 'Arithmancy'].dropna()
+    #     plt.hist(grade, alpha=0.5, label=house)
+    # plt.legend(loc='upper right')
+    # plt.show()
 # Create a boxplot as bonus
     # plt.figure(1)
     # df.boxplot('Arithmancy', by='Hogwarts House', figsize=(12, 8))
     # plt.figure(2)
     # df.boxplot('Astronomy', by='Hogwarts House', figsize=(12, 8))
     # plt.show()
-
-
     '''
         to do : 
-            - understand ANOVA
-            - find selection criteria
+            - understand ANOVA. Choose if fit (other possibility : Bertlett, Leven)
+            - find selection criteria -> p value highest.
             - list of subjects (Arithmancy, Herbology, etc...)
             - loop stats.f_oneway
             - selection
             - plot hist
             - bonus : plot boxplot
     '''
-    print(stats.f_oneway(df.loc[df['Hogwarts House'] == 'Hufflepuff', 'Herbology'].dropna(), df.loc[df['Hogwarts House'] == 'Gryffindor', 'Herbology'].dropna(), df.loc[df['Hogwarts House'] == 'Slytherin', 'Herbology'].dropna()))
-    print(stats.f_oneway(df.loc[df['Hogwarts House'] == 'Hufflepuff', 'Arithmancy'].dropna(), df.loc[df['Hogwarts House'] == 'Gryffindor', 'Arithmancy'].dropna(), df.loc[df['Hogwarts House'] == 'Slytherin', 'Arithmancy'].dropna()))
-    print(stats.f_oneway(df.loc[df['Hogwarts House'] == 'Hufflepuff', 'Astronomy'].dropna(), df.loc[df['Hogwarts House'] == 'Gryffindor', 'Astronomy'].dropna(), df.loc[df['Hogwarts House'] == 'Slytherin', 'Astronomy'].dropna()))
-    print(stats.f_oneway(df.loc[df['Hogwarts House'] == 'Hufflepuff', 'Defense Against the Dark Arts'].dropna(), df.loc[df['Hogwarts House'] == 'Gryffindor', 'Defense Against the Dark Arts'].dropna(), df.loc[df['Hogwarts House'] == 'Slytherin', 'Defense Against the Dark Arts'].dropna()))
+    subjects = list(df.select_dtypes('number').to_dict().keys())
+    subjects.remove('Index')
+    print(subjects)
+    p_values = {}
+    for subject in subjects:
+        F, p_values[subject] = stats.f_oneway(get_subject_grade_house(df, subject, 'Hufflepuff'), get_subject_grade_house(df, subject, 'Gryffindor'), get_subject_grade_house(df, subject, 'Ravenclaw'), get_subject_grade_house(df, subject, 'Slytherin'))
+    p_values = {k: v for k, v in sorted(p_values.items(), key=lambda x: x[1], reverse=True)}
+    print(p_values)
+    homogen_subject = list(p_values.keys())[0]
+    for house in houses:
+        grade = df.loc[df['Hogwarts House'] == house, homogen_subject].dropna()
+        plt.hist(grade, alpha=0.5, label=house)
+    plt.legend(loc='upper right')
+    plt.show()
