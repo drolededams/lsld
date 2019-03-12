f3fc6086fe41ba80fb0d3cfebb7aa90ae64549fd
Damien GAMEIRO <dgameiro@e1r11p1.42.fr>
2019-03-12 12:19:08 +0100

    describe bonus

--- describe.py
+++ describe.py
@@ -54,6 +54,20 @@ def describe(data):
                         + values[int(count / 2)]) / 2
     else:
         stats['50%'] = values[int((count + 1) / 2 - 1)]
+    stats['Q3-Q1 range'] = stats['75%'] - stats['25%']
+    stats['mad'] = np.sum(np.absolute(values - stats['mean'])) / count
+    stats['10%'] = percentile(0.1, count, values)
+    stats['20%'] = percentile(0.2, count, values)
+    stats['30%'] = percentile(0.3, count, values)
+    stats['40%'] = percentile(0.4, count, values)
+    stats['60%'] = percentile(0.6, count, values)
+    stats['70%'] = percentile(0.7, count, values)
+    stats['80%'] = percentile(0.8, count, values)
+    stats['90%'] = percentile(0.9, count, values)
+    svalues = [
+            item for item in clean_data.values()
+            if item >= stats['10%'] and item <= stats['90%']]
+    stats['clmean'] = sum(svalues) / len(svalues)
     return stats
 
 
@@ -63,7 +77,8 @@ def display_stats(stats):
     res = pd.DataFrame.from_dict(stats)
     res = res.reindex([
         'count', 'mean', 'std', 'min',
-        '25%', '50%', '75%', 'max', 'var', 'range'])
+        '25%', '50%', '75%', 'max', 'var', 'range', 'Q3-Q1 range', 'mad',
+        '10%', '20%', '30%', '40%', '60%', '70%', '80%', '90%', 'clmean'])
     print(res)
 
 
